9cab8724a3a48a8af2bd86312e73c7d894a1b2eb
pauloancheta <paulo.ancheta@gmail.com>
2017-12-04 16:52:26 +1100

    complete template

--- python/part_1.py
+++ python/part_1.py
@@ -1,6 +1,10 @@
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 # import dataset
 dataset = pd.read_csv('../Data_Preprocessing/Data.csv')
@@ -8,13 +12,37 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
 # preprocess
-from sklearn.preprocessing import Imputer
 
 # param strategy = default is mean
 # param axis     = 0 for column and 1 for row
 imputer = Imputer(missing_values='NaN', strategy='median', axis=0)
 imputer = imputer.fit(X[:, 1:3])
-
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
-print(X)
+# encode labels
+labelEncoder_X = LabelEncoder()
+X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])
+
+oneHotEncoder = OneHotEncoder(categorical_features=[0])
+X = oneHotEncoder.fit_transform(X).toarray()
+
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_y.fit_transform(y)
+
+# split to test and training sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# feature scaling
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+print("X_train")
+print(X_train)
+print("X_test")
+print(X_test)
+
+# print("y_train")
+# print(y_train)
+# print("y_test")
+# print(y_test)
--- r/part_1.r
+++ r/part_1.r
@@ -1,18 +1,45 @@
+# START AND STOP
 # \rf - start r session
+# \rq - close r session
+
 # \l  — send current line
+# \ae - Send file
 # \fd — send function on current line (and body below) to R
 # \se — send current selection (and echo)
 # \pe — send current paragraph (and echo)
-# \rq - close r session
 
 # :help Nvim-R
+# install.packages("caTools")
+library("caTools")
 
 dataset = read.csv("../Data_Preprocessing/Data.csv")
 
+# TAKE CARE OF MISSING DATA
 dataset$Age = ifelse(is.na(dataset$Age),
                      ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
                      dataset$Age)
 
 dataset$Salary = ifelse(is.na(dataset$Salary),
-                     ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
-                     dataset$Salary)
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
+
+# ENCODING CATEGORICAL DATA
+# factors in R is not numeric
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1,2,3))
+
+dataset$Purchased = factor(dataset$Purchased,
+                         levels = c('No', 'Yes'),
+                         labels = c(0,1))
+
+# SPLIT TRAIN SETS AND TEST SETS
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+trainingSet = subset(dataset, split == TRUE)
+testSet = subset(dataset, split == FALSE)
+
+# FEATURE SCALING
+# factors in R is not numeric so we get 'x' must be numeric
+trainingSet[,2:3] = scale(trainingSet[,2:3])
+testSet[,2:3] = scale(testSet[,2:3])
